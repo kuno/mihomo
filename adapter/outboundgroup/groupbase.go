061e48cf2f5198577062bb75d4a1967be97dc3b5
kuno <kuno.the.developer@gmail.com>
2023-08-13 13:27:49 +0800

    fix: return too earlier for Selector.GetProxies

--- adapter/outboundgroup/groupbase.go
+++ adapter/outboundgroup/groupbase.go
@@ -202,8 +202,6 @@ func (gb *GroupBase) GetProxies(touch bool) []C.Proxy {
 		}
 	}
 
-	return filterProxyByWeight(proxies, gb.weightFilter)
-
 	if len(gb.providers) > 1 && len(gb.filterRegs) > 1 {
 		var newProxies []C.Proxy
 		proxiesSet := map[string]struct{}{}
